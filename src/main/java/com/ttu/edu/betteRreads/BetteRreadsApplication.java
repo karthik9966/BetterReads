459714cf721db7f16ba13122e27c6397b1f59927
karthik9966 <navakarthikreddy@gmail.com>
2023-05-30 18:22:40 -0500

    Init Repository with dependencies

---
+++ src/main/java/com/ttu/edu/betteRreads/BetteRreadsApplication.java
@@ -0,0 +1,13 @@
+package com.ttu.edu.betteRreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetteRreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetteRreadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttu/edu/betteRreads/BetteRreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttu.edu.betteRreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetteRreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
